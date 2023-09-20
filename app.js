d4992cb0fad701e6cf41f145efebf79ed14ef49b
Luis Rondon <rondonluisgrf@gmail.com>
2023-09-20 08:48:15 -0300

    agrega funcionalidad sumar

--- app.js
+++ app.js
@@ -3,26 +3,27 @@
 const process = require("process");
 
 const { argv } = process;
+const sumar = require("./sumar")
 
-function calculadora(comando) {
+function calculadora(comando, a,b) {
     switch (comando) {
         case "sumar":
-    
+        return sumar(a,b)
             break;
         case "restar":
-    
+
             break;
         case "multiplicar":
-    
+
             break;
         case "dividir":
-    
+
             break;
-    
+
         default:
-        return "Comando no valido"
+            return "Comando no valido"
             break;
     }
 }
 
-console.log(calculadora(argv[2],argv[3],argv[4]));
+console.log(calculadora(argv[2], +argv[3], +argv[4]));
